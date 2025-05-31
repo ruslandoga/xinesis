881090c75c09dfe63915284e0b7ed1bf705c1c5b
ruslandoga <ruslandoga+gh@icloud.com>
2025-05-31 15:03:18 +0300

    add ci (#1)
    * add ci
    
    * wip
    
    * ghcr blocked me, switching to docker hub
    
    * eh?
    
    * eh!!!

--- test/kinesis_test.exs
+++ test/kinesis_test.exs
@@ -1,8 +1,17 @@
 defmodule KinesisTest do
   use ExUnit.Case
-  doctest Kinesis
 
-  test "greets the world" do
-    assert Kinesis.hello() == :world
+  test "it works" do
+    {:ok, _pid} = Kinesis.start_link([])
+  end
+
+  test "list_shards/1" do
+    {:ok, conn} = Mint.HTTP1.connect(:http, "localhost", 4566, mode: :passive)
+    assert {:ok, _conn, [200, _headers, body]} = Kinesis.list_shards(conn)
+
+    assert JSON.decode!(body) == %{
+             "HasMoreStreams" => false,
+             "StreamNames" => ["my-local-stream"]
+           }
   end
 end
