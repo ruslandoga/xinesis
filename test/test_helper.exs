7045015fccb9447eb9ac018724aa460263010c06
ruslandoga <ruslandoga+gh@icloud.com>
2025-05-31 12:52:33 +0300

    add ci

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,18 @@
+kinesis_available? =
+  case :httpc.request(:get, {~c"http://localhost:8123/ping", []}, [], []) do
+    {:ok, {{_version, _status = 200, _reason}, _headers, ~c"Ok.\n"}} ->
+      true
+
+    {:error, {:failed_connect, [{:to_address, _to_address}, {:inet, [:inet], :econnrefused}]}} ->
+      false
+  end
+
+unless kinesis_available? do
+  Mix.shell().error("""
+  Kinesis is not detected! Please start the local container with the following command:
+
+      docker compose up -d kinesis
+  """)
+end
+
 ExUnit.start()
