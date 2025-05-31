8fed66e7bb996af0af798b228ac18ec487059c60
ruslandoga <ruslandoga+gh@icloud.com>
2025-05-31 18:19:40 +0300

    continue

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 wip
+
+TODOs:
+- [ ] https://docs.localstack.cloud/user-guide/aws/kinesis/
+- [ ] https://docs.localstack.cloud/user-guide/aws/dynamodb/
+- [ ] https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/DynamoDBLocal.html
+- [ ] https://gist.github.com/etspaceman/137f7f540af32bf106873813c830a699
+- [ ] https://docs.aws.amazon.com/streams/latest/dev/introduction.html
+- [ ] https://github.com/uberbrodt/kcl_ex
+- [ ] https://github.com/awslabs/amazon-kinesis-client (tests)
+- [ ] https://docs.aws.amazon.com/amazondynamodb/latest/APIReference/API_Operations_Amazon_DynamoDB.html for leases and checkpoints
+- [ ] https://docs.aws.amazon.com/kinesis/latest/APIReference/API_Operations.html
--- test/kinesis_test.exs
+++ test/kinesis_test.exs
@@ -1,133 +1,422 @@
 defmodule KinesisTest do
   use ExUnit.Case
 
-  test "basic flow" do
-    {:ok, conn} = Mint.HTTP1.connect(:http, "localhost", 4566, mode: :passive)
+  setup %{test: test} do
+    test_base64 = Base.encode64(Atom.to_string(test), padding: false)
+    stream_name = "test_stream_#{test_base64}"
+    table_name = "test_table_#{test_base64}"
+    {:ok, conn: conn(), stream_name: stream_name, table_name: table_name}
+  end
 
-    assert {:ok, conn, resp} =
-             Kinesis.dynamodb_create_table(conn, %{
-               "AttributeDefinitions" => [%{"AttributeName" => "id", "AttributeType" => "S"}],
-               "KeySchema" => [%{"AttributeName" => "id", "KeyType" => "HASH"}],
-               "ProvisionedThroughput" => %{
-                 "ReadCapacityUnits" => 5,
-                 "WriteCapacityUnits" => 5
-               },
-               "TableName" => "my-test-table"
-             })
+  describe "basic flow & iterator logic" do
+    setup [:create_stream, :await_stream_active]
 
-    on_exit(fn ->
-      {:ok, conn} = Mint.HTTP1.connect(:http, "localhost", 4566, mode: :passive)
-      {:ok, _, _} = Kinesis.dynamodb_delete_table(conn, %{"TableName" => "my-test-table"})
-    end)
+    test "consume all records with pagination", %{conn: conn, stream_name: stream_name} do
+      {:ok, conn, %{"FailedRecordCount" => 0}} =
+        Kinesis.api_put_records(conn, %{
+          "StreamName" => stream_name,
+          "Records" =>
+            Enum.map(1..100, fn i ->
+              %{
+                "Data" => Base.encode64("data-#{i}"),
+                "PartitionKey" => "key-#{i}"
+              }
+            end)
+        })
 
-    assert %{
-             "TableDescription" => %{
-               "AttributeDefinitions" => [%{"AttributeName" => "id", "AttributeType" => "S"}],
-               "CreationDateTime" => _,
-               "DeletionProtectionEnabled" => false,
-               "ItemCount" => 0,
-               "KeySchema" => [%{"AttributeName" => "id", "KeyType" => "HASH"}],
-               "ProvisionedThroughput" => %{
-                 "NumberOfDecreasesToday" => 0,
-                 "ReadCapacityUnits" => 5,
-                 "WriteCapacityUnits" => 5
-               },
-               "TableArn" => "arn:aws:dynamodb:us-east-1:000000000000:table/my-test-table",
-               "TableId" => _,
-               "TableName" => "my-test-table",
-               "TableSizeBytes" => 0,
-               "TableStatus" => "ACTIVE"
-             }
-           } = resp
-
-    assert {:ok, conn, resp} =
-             Kinesis.kinesis_create_stream(conn, %{
-               "StreamName" => "my-test-stream",
-               "ShardCount" => 1
-             })
-
-    on_exit(fn ->
-      {:ok, conn} = Mint.HTTP1.connect(:http, "localhost", 4566, mode: :passive)
-      {:ok, _, _} = Kinesis.kinesis_delete_stream(conn, %{"StreamName" => "my-test-stream"})
-    end)
-
-    assert resp == %{}
-
-    assert {:ok, conn, resp} = Kinesis.kinesis_list_streams(conn, %{})
-
-    assert %{
-             "HasMoreStreams" => false,
-             "StreamNames" => streams
-           } = resp
-
-    assert "my-test-stream" in streams
-
-    assert {:ok, conn, response} =
-             Kinesis.kinesis_describe_stream(conn, %{"StreamName" => "my-test-stream"})
-
-    assert %{
-             "StreamDescription" => %{
-               "EncryptionType" => "NONE",
-               "EnhancedMonitoring" => [%{"ShardLevelMetrics" => []}],
-               "HasMoreShards" => false,
-               "RetentionPeriodHours" => 24,
-               "Shards" => [
-                 %{
-                   "HashKeyRange" => %{
-                     "EndingHashKey" => _ending_hash_key,
-                     "StartingHashKey" => "0"
-                   },
-                   "SequenceNumberRange" => %{
-                     "StartingSequenceNumber" => starting_sequence_number
-                   },
-                   "ShardId" => "shardId-000000000000"
-                 }
-               ],
-               "StreamARN" => "arn:aws:kinesis:us-east-1:000000000000:stream/my-test-stream",
-               "StreamCreationTimestamp" => _,
-               "StreamModeDetails" => %{"StreamMode" => "PROVISIONED"},
-               "StreamName" => "my-test-stream",
-               # TODO await ACTIVE?
-               "StreamStatus" => "CREATING"
-             }
-           } = response
-
-    # TODO?
-    :timer.sleep(550)
-
-    assert {:ok, conn, %{"StreamDescription" => %{"StreamStatus" => "ACTIVE"}}} =
-             Kinesis.kinesis_describe_stream(conn, %{"StreamName" => "my-test-stream"})
-
-    assert {:ok, conn, resp} =
-             Kinesis.kinesis_get_shard_iterator(
-               conn,
-               %{
-                 "StreamName" => "my-test-stream",
+      assert {:ok, conn, %{"ShardIterator" => shard_iterator}} =
+               Kinesis.api_get_shard_iterator(conn, %{
+                 "StreamName" => stream_name,
                  "ShardId" => "shardId-000000000000",
-                 "ShardIteratorType" => "AT_SEQUENCE_NUMBER",
-                 "StartingSequenceNumber" => starting_sequence_number
-               }
-             )
+                 "ShardIteratorType" => "TRIM_HORIZON"
+               })
 
-    assert %{
-             "ShardIterator" => shard_iterator
-           } = resp
+      assert {:ok, conn, resp} =
+               Kinesis.api_get_records(conn, %{
+                 "Limit" => 10,
+                 "ShardIterator" => shard_iterator
+               })
 
-    assert {:ok, conn, resp} =
-             Kinesis.kinesis_get_records(conn, %{"ShardIterator" => shard_iterator})
+      assert %{"Records" => records, "NextShardIterator" => shard_iterator} = resp
+      assert length(records) == 10
 
-    assert %{
-             "MillisBehindLatest" => 0,
-             "NextShardIterator" => _next,
-             "Records" => []
-           } = resp
+      assert {:ok, conn, resp} =
+               Kinesis.api_get_records(conn, %{
+                 "Limit" => 50,
+                 "ShardIterator" => shard_iterator
+               })
 
-    assert {:ok, conn, resp} = Kinesis.dynamodb_list_tables(conn, %{})
+      assert %{"Records" => records, "NextShardIterator" => shard_iterator} = resp
+      assert length(records) == 50
 
-    assert %{"TableNames" => tables} = resp
-    assert "my-test-table" in tables
+      assert {:ok, conn, resp} =
+               Kinesis.api_get_records(conn, %{
+                 "Limit" => 50,
+                 "ShardIterator" => shard_iterator
+               })
 
-    # just to avoid warnings for now
-    _conn = conn
+      assert %{"Records" => records, "NextShardIterator" => shard_iterator} = resp
+      assert length(records) == 40
+
+      assert {:ok, _conn, resp} =
+               Kinesis.api_get_records(conn, %{
+                 "Limit" => 50,
+                 "ShardIterator" => shard_iterator
+               })
+
+      assert %{"Records" => []} = resp
+    end
+
+    test "shard iterator types", %{conn: conn, stream_name: stream_name} do
+      {:ok, conn, %{"SequenceNumber" => seq1, "ShardId" => "shardId-000000000000"}} =
+        Kinesis.api_put_record(conn, %{
+          "StreamName" => stream_name,
+          "Data" => Base.encode64("record-1"),
+          "PartitionKey" => "key"
+        })
+
+      {:ok, conn, %{"SequenceNumber" => seq2, "ShardId" => "shardId-000000000000"}} =
+        Kinesis.api_put_record(conn, %{
+          "StreamName" => stream_name,
+          "Data" => Base.encode64("record-2"),
+          "PartitionKey" => "key"
+        })
+
+      # TRIM_HORIZON gets all records
+      assert {:ok, conn, %{"ShardIterator" => shard_iterator_trim}} =
+               Kinesis.api_get_shard_iterator(conn, %{
+                 "StreamName" => stream_name,
+                 "ShardId" => "shardId-000000000000",
+                 "ShardIteratorType" => "TRIM_HORIZON"
+               })
+
+      assert {:ok, conn,
+              %{"Records" => [%{"SequenceNumber" => ^seq1}, %{"SequenceNumber" => ^seq2}]}} =
+               Kinesis.api_get_records(conn, %{"ShardIterator" => shard_iterator_trim})
+
+      # LATEST gets no records ...
+      assert {:ok, conn, %{"ShardIterator" => shart_iterator_latest}} =
+               Kinesis.api_get_shard_iterator(conn, %{
+                 "StreamName" => stream_name,
+                 "ShardId" => "shardId-000000000000",
+                 "ShardIteratorType" => "LATEST"
+               })
+
+      assert {:ok, conn, %{"Records" => []}} =
+               Kinesis.api_get_records(conn, %{"ShardIterator" => shart_iterator_latest})
+
+      # ... until a new record is added ...
+      {:ok, conn, %{"SequenceNumber" => seq3, "ShardId" => "shardId-000000000000"}} =
+        Kinesis.api_put_record(conn, %{
+          "StreamName" => stream_name,
+          "Data" => Base.encode64("record-3"),
+          "PartitionKey" => "key"
+        })
+
+      # ... and then it gets the latest record
+      assert {:ok, conn, %{"Records" => [%{"SequenceNumber" => ^seq3}]}} =
+               Kinesis.api_get_records(conn, %{"ShardIterator" => shart_iterator_latest})
+
+      # AT_SEQUENCE_NUMBER
+      {:ok, conn, %{"ShardIterator" => shard_iterator_at_seq}} =
+        Kinesis.api_get_shard_iterator(conn, %{
+          "StreamName" => stream_name,
+          "ShardId" => "shardId-000000000000",
+          "ShardIteratorType" => "AT_SEQUENCE_NUMBER",
+          "StartingSequenceNumber" => seq1
+        })
+
+      assert {:ok, _conn,
+              %{
+                "Records" => [
+                  %{"SequenceNumber" => ^seq1},
+                  %{"SequenceNumber" => ^seq2},
+                  %{"SequenceNumber" => ^seq3}
+                ]
+              }} =
+               Kinesis.api_get_records(conn, %{"ShardIterator" => shard_iterator_at_seq})
+
+      # AFTER_SEQUENCE_NUMBER
+      # {:ok, conn, %{"ShardIterator" => shard_iterator_after_seq}} =
+      #   Kinesis.api_get_shard_iterator(conn, %{
+      #     "StreamName" => stream_name,
+      #     "ShardId" => "shardId-000000000000",
+      #     "ShardIteratorType" => "AFTER_SEQUENCE_NUMBER",
+      #     "StartingSequenceNumber" => seq1
+      #   })
+
+      # assert {:ok, _conn,
+      #         %{
+      #           "Records" => [
+      #             %{"SequenceNumber" => ^seq2},
+      #             %{"SequenceNumber" => ^seq3}
+      #           ]
+      #         }} =
+      #          Kinesis.api_get_records(conn, %{"ShardIterator" => shard_iterator_after_seq})
+
+      # TODO AT_TIMESTAMP ???
+    end
+
+    @tag :skip
+    test "expired shard iterator"
+  end
+
+  describe "checkpointing" do
+    setup [:create_stream, :await_stream_active, :create_table]
+
+    test "processing records and updating checkpoint", %{
+      conn: conn,
+      stream_name: stream_name,
+      table_name: table_name
+    } do
+      owner = "uniq_lease_owner"
+
+      {:ok, conn, %{"FailedRecordCount" => 0}} =
+        Kinesis.api_put_records(conn, %{
+          "StreamName" => stream_name,
+          "Records" =>
+            Enum.map(1..100, fn i ->
+              %{
+                "Data" => Base.encode64("data-#{i}"),
+                "PartitionKey" => "key-#{i}"
+              }
+            end)
+        })
+
+      assert {:ok, conn, %{"Shards" => [%{"ShardId" => shard_id}]}} =
+               Kinesis.api_list_shards(conn, %{"StreamName" => stream_name})
+
+      assert {:ok, conn, _not_found = %{}} =
+               Kinesis.get_lease(conn, table_name, shard_id, owner)
+
+      assert {:ok, conn, _ok = %{}} =
+               Kinesis.create_lease(conn, table_name, shard_id, owner)
+
+      assert {:ok, conn, %{"Item" => lease}} =
+               Kinesis.get_lease(conn, table_name, shard_id, owner)
+
+      assert lease == %{
+               "completed" => %{"BOOL" => false},
+               "lease_count" => %{"N" => "1"},
+               "lease_owner" => %{"S" => owner},
+               "shard_id" => %{"S" => shard_id}
+             }
+
+      assert {:ok, conn, %{"ShardIterator" => shard_iterator}} =
+               Kinesis.api_get_shard_iterator(conn, %{
+                 "StreamName" => stream_name,
+                 "ShardId" => shard_id,
+                 "ShardIteratorType" => "TRIM_HORIZON"
+               })
+
+      assert {:ok, conn, %{"NextShardIterator" => shard_iterator, "Records" => [_ | _]}} =
+               Kinesis.api_get_records(conn, %{
+                 "Limit" => 100,
+                 "ShardIterator" => shard_iterator
+               })
+
+      assert {:ok, _conn, %{"Attributes" => %{"checkpoint" => %{"S" => ^shard_iterator}}}} =
+               Kinesis.update_checkpoint(conn, table_name, shard_id, owner, shard_iterator)
+    end
+
+    @tag :skip
+    test "resuming processing from checkpoint"
+    @tag :skip
+    test "failed checkpoint write"
+  end
+
+  describe "shard splitting & merging" do
+    setup [:create_stream, :await_stream_active, :create_table]
+
+    test "shard split", %{conn: conn, stream_name: stream_name} do
+      {:ok, conn, %{"FailedRecordCount" => 0}} =
+        Kinesis.api_put_records(conn, %{
+          "StreamName" => stream_name,
+          "Records" =>
+            Enum.map(1..10, fn i ->
+              %{
+                "Data" => Base.encode64("data-#{i}"),
+                "PartitionKey" => "key-#{i}"
+              }
+            end)
+        })
+
+      {:ok, conn,
+       %{
+         "Shards" => [
+           %{
+             "ShardId" => shard_id,
+             "HashKeyRange" => %{
+               "EndingHashKey" => ending_hash_key,
+               "StartingHashKey" => starting_hash_key
+             }
+           }
+         ]
+       }} = Kinesis.api_list_shards(conn, %{"StreamName" => stream_name})
+
+      starting_hash_key = String.to_integer(starting_hash_key)
+      ending_hash_key = String.to_integer(ending_hash_key)
+
+      assert {:ok, conn, %{}} =
+               Kinesis.api_split_shard(conn, %{
+                 "StreamName" => stream_name,
+                 "ShardToSplit" => shard_id,
+                 "NewStartingHashKey" =>
+                   Integer.to_string(div(starting_hash_key + ending_hash_key, 2))
+               })
+
+      assert {:ok, conn,
+              %{
+                "Shards" => [
+                  %{
+                    "HashKeyRange" => %{
+                      "EndingHashKey" => "340282366920938463463374607431768211455",
+                      "StartingHashKey" => "0"
+                    },
+                    "ShardId" => ^shard_id
+                  },
+                  %{
+                    "HashKeyRange" => %{
+                      "EndingHashKey" => "170141183460469231731687303715884105726",
+                      "StartingHashKey" => "0"
+                    },
+                    "ParentShardId" => ^shard_id,
+                    "ShardId" => "shardId-000000000001"
+                  },
+                  %{
+                    "HashKeyRange" => %{
+                      "EndingHashKey" => "340282366920938463463374607431768211455",
+                      "StartingHashKey" => "170141183460469231731687303715884105727"
+                    },
+                    "ParentShardId" => ^shard_id,
+                    "ShardId" => "shardId-000000000002"
+                  }
+                ]
+              }} = Kinesis.api_list_shards(conn, %{"StreamName" => stream_name})
+
+      assert {:ok, conn, %{"ShardIterator" => shard_iterator}} =
+               Kinesis.api_get_shard_iterator(conn, %{
+                 "StreamName" => stream_name,
+                 "ShardId" => shard_id,
+                 "ShardIteratorType" => "TRIM_HORIZON"
+               })
+
+      assert {:ok, conn, %{"NextShardIterator" => shard_iterator, "Records" => [_ | _]}} =
+               Kinesis.api_get_records(conn, %{
+                 "Limit" => 5,
+                 "ShardIterator" => shard_iterator
+               })
+
+      assert {:ok, _conn,
+              %{
+                "ChildShards" => [
+                  %{"ShardId" => "shardId-000000000001"},
+                  %{"ShardId" => "shardId-000000000002"}
+                ],
+                "Records" => [_ | _]
+              }} =
+               Kinesis.api_get_records(conn, %{
+                 "Limit" => 5,
+                 "ShardIterator" => shard_iterator
+               })
+    end
+
+    @tag :skip
+    test "shard merge"
+  end
+
+  describe "error handling and edge cases" do
+    @describetag :skip
+    test "stream not found"
+    test "DynamoDB table for checkpoints not found"
+    test "throttling (ProvisionedThroughputExceededException)"
+    test "processing empty GetRecords response"
+    test "stream deletion and recreation"
+  end
+
+  # todo https://github.com/uberbrodt/kcl_ex/blob/master/lib/kinesis_client/stream/app_state/dynamo.ex
+
+  defp conn do
+    {:ok, conn} = Mint.HTTP1.connect(:http, "localhost", 4566, mode: :passive)
+    conn
+  end
+
+  defp with_conn(f) when is_function(f, 1) do
+    conn = conn()
+
+    try do
+      f.(conn)
+    after
+      Mint.HTTP1.close(conn)
+    end
+  end
+
+  defp create_stream(%{stream_name: stream_name} = ctx) do
+    with_conn(fn conn ->
+      {:ok, _, _} =
+        Kinesis.api_create_stream(conn, %{"StreamName" => stream_name, "ShardCount" => 1})
+    end)
+
+    on_exit(fn ->
+      :ok = await_stream_active(ctx)
+
+      with_conn(fn conn ->
+        {:ok, _, _} = Kinesis.api_delete_stream(conn, %{"StreamName" => stream_name})
+      end)
+    end)
+  end
+
+  defp await_stream_active(%{stream_name: stream_name}) do
+    :active =
+      with_conn(fn conn ->
+        delays = List.duplicate(100, 10)
+
+        Enum.reduce_while(delays, conn, fn delay, conn ->
+          {:ok, conn, resp} = Kinesis.api_describe_stream(conn, %{"StreamName" => stream_name})
+          %{"StreamDescription" => %{"StreamStatus" => status}} = resp
+
+          if status == "ACTIVE" do
+            {:halt, :active}
+          else
+            :timer.sleep(delay)
+            {:cont, conn}
+          end
+        end)
+      end)
+
+    :ok
+  end
+
+  defp create_table(%{table_name: table_name}) do
+    with_conn(fn conn ->
+      {:ok, _, _} =
+        Kinesis.dynamodb_create_table(conn, %{
+          "TableName" => table_name,
+          "KeySchema" => [
+            %{
+              "AttributeName" => "shard_id",
+              "KeyType" => "HASH"
+            }
+          ],
+          "AttributeDefinitions" => [
+            %{
+              "AttributeName" => "shard_id",
+              "AttributeType" => "S"
+            }
+          ],
+          "ProvisionedThroughput" => %{
+            "ReadCapacityUnits" => 10,
+            "WriteCapacityUnits" => 10
+          }
+        })
+    end)
+
+    on_exit(fn ->
+      with_conn(fn conn ->
+        {:ok, _, _} = Kinesis.dynamodb_delete_table(conn, %{"TableName" => table_name})
+      end)
+    end)
+
+    :ok
   end
 end
