1b0057a020688df6e96700e814eeb8bbf6917208
ruslandoga <ruslandoga+gh@icloud.com>
2025-07-26 13:15:27 +0300

    mix new

---
+++ lib/xinesis.ex
@@ -0,0 +1,18 @@
+defmodule Xinesis do
+  @moduledoc """
+  Documentation for `Xinesis`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Xinesis.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/xinesis_test.exs
@@ -0,0 +1,8 @@
+defmodule XinesisTest do
+  use ExUnit.Case
+  doctest Xinesis
+
+  test "greets the world" do
+    assert Xinesis.hello() == :world
+  end
+end
