d2b0593cc64cdb6a24745cdd8a18a51fb01d113e
ruslandoga <ruslandoga+gh@icloud.com>
2025-07-28 22:01:09 +0300

    use aws in coordinator/processor test

--- test/xinesis/some_other_test.exs
+++ test/xinesis/some_other_test.exs
@@ -2,38 +2,38 @@ defmodule Xinesis.SomeOtherTest do
   use ExUnit.Case
   alias Xinesis.AWS
 
-  @moduletag :localstack
+  @moduletag :aws
 
-  @localstack_kinesis [
-    scheme: :http,
-    host: "localhost",
-    port: 4566,
-    region: "us-east-1",
-    access_key_id: "test",
-    secret_access_key: "test"
-  ]
+  def kinesis_config do
+    [
+      scheme: :https,
+      host: "kinesis.eu-north-1.api.aws",
+      port: 443,
+      region: "eu-north-1",
+      access_key_id: System.fetch_env!("ACCESS_KEY_ID"),
+      secret_access_key: System.fetch_env!("SECRET_ACCESS_KEY")
+    ]
+  end
 
   setup do
-    stream = "some-other-test-stream-#{System.unique_integer([:positive])}"
+    stream = "knock-elixir-ci-test-stream-some-other-test"
 
-    # Xinesis.Test.with_conn(@localstack_kinesis, fn conn ->
-    #   AWS.delete_stream(conn, %{"StreamName" => stream})
-    # end)
-
-    Xinesis.Test.with_conn(@localstack_kinesis, fn conn ->
-      AWS.create_stream(conn, %{"ShardCount" => 4, "StreamName" => stream})
+    Xinesis.Test.with_conn(kinesis_config(), fn conn ->
+      AWS.create_stream(conn, %{"ShardCount" => 1, "StreamName" => stream})
     end)
 
     on_exit(fn ->
-      Xinesis.Test.with_conn(@localstack_kinesis, fn conn ->
+      Xinesis.Test.with_conn(kinesis_config(), fn conn ->
         AWS.delete_stream(conn, %{"StreamName" => stream})
       end)
     end)
 
-    %{"StreamDescriptionSummary" => %{"StreamARN" => stream_arn, "StreamStatus" => "ACTIVE"}} =
-      Xinesis.Test.with_conn(@localstack_kinesis, fn conn ->
+    %{"StreamDescriptionSummary" => %{"StreamARN" => stream_arn}} =
+      Xinesis.Test.with_conn(kinesis_config(), fn conn ->
         AWS.describe_stream_summary(conn, %{"StreamName" => stream})
       end)
+
+    Xinesis.Test.await_stream_active(kinesis_config(), stream_arn)
 
     {:ok, stream_arn: stream_arn}
   end
@@ -41,41 +41,36 @@ defmodule Xinesis.SomeOtherTest do
   test "it works", %{stream_arn: stream_arn} do
     test = self()
 
-    start_supervised!(
-      {Xinesis,
-       name: __MODULE__,
-       stream_arn: stream_arn,
-       scheme: :http,
-       host: "localhost",
-       port: 4566,
-       region: "us-east-1",
-       access_key_id: "test",
-       secret_access_key: "test",
-       processor: fn shard_id, records ->
-         send(test, {:records, shard_id, records})
-       end}
-    )
+    config =
+      Keyword.merge(kinesis_config(),
+        name: __MODULE__,
+        stream_arn: stream_arn,
+        processor: fn shard_id, records ->
+          send(test, {:records, shard_id, records})
+        end
+      )
 
-    Xinesis.Test.with_conn(@localstack_kinesis, fn conn ->
+    start_supervised!({Xinesis, config})
+
+    Xinesis.Test.with_conn(kinesis_config(), fn conn ->
       AWS.put_record(conn, %{
         "StreamARN" => stream_arn,
-        "Data" => Base.encode64("some data and stuff"),
-        "PartitionKey" => "test-key"
+        "Data" => "AA==",
+        "PartitionKey" => "test-key-0"
       })
     end)
 
-    assert_receive {:records, "shardId-000000000000", records}
+    assert_receive {:records, "shardId-000000000000", records}, :timer.seconds(5)
 
     assert [
              %{
                "ApproximateArrivalTimestamp" => _,
-               "Data" => "c29tZSBkYXRhIGFuZCBzdHVmZg==",
-               "EncryptionType" => "NONE",
-               "PartitionKey" => "test-key",
+               "Data" => "AA==",
+               "PartitionKey" => "test-key-0",
                "SequenceNumber" => _
              }
            ] = records
 
-    refute_receive _anything, 2000
+    assert_receive {:records, "shardId-000000000000", []}, :timer.seconds(5)
   end
 end
